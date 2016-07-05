b2b5bf09102ab541a598ed129503b34ced31f70c
wei qi <weiqi@hzdracom.com>
2016-07-05 21:07:28 +0800

    新建分组

--- RACLogin/Classes/WQRegisDemoViewController.m
+++ RACLogin/Classes/WQRegisDemoViewController.m
@@ -43,7 +43,6 @@
                     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                         [MBProgressHUD hudHideForView:self.view];
                     });
-                    //
                     
                     
                     
